394c23b765149d5aa17a963982a2f1842d3fb636
Kavan Tan <kavantan10@gmail.com>
2024-03-04 08:09:43 +0800

    Implement routing capabilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AboutUsPage from './pages/AboutUsPage';
+import CommunityPage from './pages/CommunityPage';
+import ContactUsPage from './pages/ContactUsPage';
+import CulturesPage from './pages/CulturesPage';
 import MainPage from './pages/MainPage/MainPage';
 
 function App() {
   return (
-    <div>
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/community" element={<CommunityPage />} />
+        <Route path="/contact-us" element={<ContactUsPage />} />
+        <Route path="/cultures" element={<CulturesPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,50 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>CultureWeb</div>
       <div className={styles.navLinks}>
-        <button className={styles.navItem} type="button">
+        <button
+          type="button"
+          onClick={() => handleNavigate('/')}
+          className={styles.navItem}
+        >
           Home
         </button>
-        <button className={styles.navItem} type="button">
+        <button
+          type="button"
+          onClick={() => handleNavigate('/cultures')}
+          className={styles.navItem}
+        >
           Cultures
         </button>
-        <button className={styles.navItem} type="button">
+        <button
+          type="button"
+          onClick={() => handleNavigate('/about-us')}
+          className={styles.navItem}
+        >
           About Us
         </button>
-        <button className={styles.navItem} type="button">
+        <button
+          type="button"
+          onClick={() => handleNavigate('/community')}
+          className={styles.navItem}
+        >
           Community
         </button>
-        <button className={styles.navItem} type="button">
+        <button
+          type="button"
+          onClick={() => handleNavigate('/contact-us')}
+          className={styles.navItem}
+        >
           Contact Us
         </button>
       </div>
