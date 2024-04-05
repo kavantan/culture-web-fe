764c70f8d7e333f2e361eb17a6be029642bd7259
Kavan Tan <kavantan10@gmail.com>
2024-04-05 22:11:02 +0800

    Add image to DL algorithm display

--- src/components/ContentRender/RenderCharacterContent/RenderCharacterContent.jsx
+++ src/components/ContentRender/RenderCharacterContent/RenderCharacterContent.jsx
@@ -1,3 +1,4 @@
+import * as characterImages from './images';
 import charactersData from '../../../assets/data/characters.json';
 import styles from './RenderCharacterContent.module.css';
 
@@ -5,10 +6,18 @@ function RenderCharacterContent(output) {
   const characterInfo = charactersData.find(
     (char) => char.name.toLowerCase() === output?.prediction.toLowerCase(),
   );
+
+  const characterImage = characterImages[characterInfo?.name.replace(/-/g, '')];
+
   return (
     characterInfo && (
       <div className={styles.output}>
         <h3>{characterInfo.name}</h3>
+        <img
+          src={characterImage}
+          alt={characterInfo.name}
+          className={styles.characterImage}
+        />
         <p>{characterInfo.description}</p>
         <button
           type="button"
