25ce4759e140136c26802a15d4518aecabc1fc8d
Kavan Tan <kavantan10@gmail.com>
2024-04-05 22:42:46 +0800

    Modify DL algorithm output

--- src/components/ContentRender/RenderCharacterContent/RenderCharacterContent.jsx
+++ src/components/ContentRender/RenderCharacterContent/RenderCharacterContent.jsx
@@ -1,5 +1,6 @@
 import * as characterImages from './images';
 import charactersData from '../../../assets/data/characters.json';
+import toPascalCase from '../../../utils/toPascalCase';
 import styles from './RenderCharacterContent.module.css';
 
 function RenderCharacterContent(output) {
@@ -7,18 +8,33 @@ function RenderCharacterContent(output) {
     (char) => char.name.toLowerCase() === output?.prediction.toLowerCase(),
   );
 
-  const characterImage = characterImages[characterInfo?.name.replace(/-/g, '')];
+  const characterImage =
+    characterImages[toPascalCase(characterInfo?.name || '')];
 
   return (
     characterInfo && (
       <div className={styles.output}>
-        <h3>{characterInfo.name}</h3>
         <img
           src={characterImage}
           alt={characterInfo.name}
           className={styles.characterImage}
         />
-        <p>{characterInfo.description}</p>
+        <table className={styles.characterTable}>
+          <tbody>
+            <tr>
+              <th>Name:</th>
+              <td>{characterInfo.name}</td>
+            </tr>
+            <tr>
+              <th>Description:</th>
+              <td>{characterInfo.shortDescription}</td>
+            </tr>
+            <tr>
+              <th>Examples:</th>
+              <td>{characterInfo.examples}</td>
+            </tr>
+          </tbody>
+        </table>
         <button
           type="button"
           className={styles.findOutMoreButton}
