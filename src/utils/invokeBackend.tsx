669b6d8b12a0d1b97f63bee0fd8beb258ce75774
Zong Xian <48787208+kangzongxian@users.noreply.github.com>
2024-09-18 09:13:04 +0800

    Update character recognition
    Update character recognition

--- src/utils/invokeBackend.tsx
+++ src/utils/invokeBackend.tsx
@@ -31,37 +31,6 @@ export const uploadImgToExpressionRecBE = async (
 ): Promise<PredictionMultiple> => {
   const formData = new FormData();
   formData.append('image', imageFile);
-  if (imageFile.name === 'ImageTest1.jpg') {
-    return {prediction: [
-      {prediction: 'Odious',
-        location: {
-          x: 430,
-          y: 231,
-          width: 1500,
-          height: 1500,
-          probability: 0
-        }
-      }
-    ]
-    }
-  }
-  if (imageFile.name === 'ImageTest2.jpg') {
-    return {prediction: [
-      {prediction: 'Wonder',
-        location: {
-          x: 430,
-          y: 52,
-          width: 1500,
-          height: 1500,
-          probability: 0
-        }
-      }
-    ]
-    }
-
-  }
-
-
   const response = await fetch(`${BACKEND_URI}/classify-expression`, {
     method: 'POST',
     body: formData,
@@ -69,33 +38,6 @@ export const uploadImgToExpressionRecBE = async (
     .catch(() => {
       throw new Error('Failed to upload image.');
   });
-  if (response[0].prediction === 'Terrible') {
-    return {prediction: [
-      {prediction: 'Terrible',
-        location: {
-          x: 430,
-          y: 231,
-          width: 1500,
-          height: 1500,
-          probability: 0
-        }
-      }
-    ]
-    }}
-
-    if (response[0].prediction === 'Peace') {
-      return {prediction: [
-        {prediction: 'Peace',
-          location: {
-            x: 674,
-            y: 52,
-            width: 1500,
-            height: 1500,
-            probability: 0
-          }
-        }
-      ]
-      }}
 
 
   const toReturn: Prediction[] = response.map((value: { prediction: string; location: Location[]; accuracy: number[]; }) => ({
@@ -111,24 +53,30 @@ export const uploadImgToExpressionRecBE = async (
   return {prediction: toReturn};
 };
 
-export const uploadImgToCharRecBE = async (
-  imageFile: File,
-): Promise<PredictionMultiple> => {
-  const formData = new FormData();
-  formData.append('image', imageFile);
-
-  const response = await fetch(`${BACKEND_URI}/classify`, {
-    method: 'POST',
-    body: formData,
-  });
-
-  if (!response.ok) {
-    throw new Error('Failed to upload image.');
-  }
-
-  // TODO: Uncomment return statement and test function once backend is ready
-  // return response.json();
-  return mockResponse;
+export const uploadImgToCharRecBE = async ( 
+  imageFile: File, 
+): Promise<PredictionMultiple> => { 
+  const formData = new FormData(); 
+  formData.append('image', imageFile); 
+  const response = await fetch(`${BACKEND_URI}/`, { 
+    method: 'POST', 
+    body: formData, 
+  }).then((output) => output.json()) 
+    .catch(() => { 
+      throw new Error('Failed to upload image.'); 
+  }); 
+ 
+  const toReturn: Prediction[] = response.map((value: { prediction: string; location: Location[]; accuracy: number[]; }) => ({ 
+      prediction: value.prediction, 
+      location: { 
+        x: value.location[0], 
+        y: value.location[2], 
+        width: value.location[1], 
+        height: value.location[3], 
+        probability: value.accuracy 
+      } 
+    })) 
+  return {prediction: toReturn}; 
 };
 
 // TODO: Uncomment and test function once backend is ready
